0ad083ec1b1f715673d190ac247fac4081e5bf3c
amozalev <mozalev.alexey@gmail.com>
2022-08-02 23:01:38 +0300

    Implemented sortStrings.

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,45 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const sortedArr = [...arr];
 
+  const strQuickSort = (arr, left = 0, right = arr.length) => {
+    if (left >= right) {
+      return;
+    }
+
+    const pivotInd = findPivotInd(arr, param, left, right);
+    strQuickSort(arr, left, pivotInd);
+    strQuickSort(arr, pivotInd + 1, right);
+  };
+  strQuickSort(sortedArr);
+
+  return sortedArr;
 }
+
+const strComparisonFn = (str1, str2, sortOrder) => {
+  return str1.localeCompare(str2, ['ru', 'en'],
+    {caseFirst: sortOrder === 'asc' ? 'upper' : 'lower'}) === (sortOrder === 'asc' ? -1 : 1);
+};
+
+const findPivotInd = (arr, sortOrder, left, right) => {
+  let swapInd = left;
+  // let pivotInd = left;
+  let pivotInd = Math.floor(Math.random() * (right - left) + left);
+  [arr[pivotInd], arr[left]] = [arr[left], arr[pivotInd]];
+  pivotInd = left;
+  let pivot = arr[pivotInd];
+
+  for (let i = left + 1; i < right; i++) {
+    if (strComparisonFn(arr[i], pivot, sortOrder)) {
+      swapInd++;
+      [arr[i], arr[swapInd]] = [arr[swapInd], arr[i]];
+    }
+  }
+  if (swapInd !== pivotInd) {
+    [arr[pivotInd], arr[swapInd]] = [arr[swapInd], arr[pivotInd]];
+  }
+
+  return swapInd;
+};
+
