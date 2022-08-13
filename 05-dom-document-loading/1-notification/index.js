84ce41b04f673623bc9a190349305d2b1b994a51
amozalev <mozalev.alexey@gmail.com>
2022-08-13 15:40:30 +0300

    Implemented NotificationMessage  component.

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,54 @@
 export default class NotificationMessage {
+  constructor(message = '', {duration = 3000, type = 'success'} = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
 
+    this.render();
+
+    this.remove = this.remove.bind(this);
+  }
+
+  get durationInSeconds() {
+    return this.duration / 1000;
+  }
+
+  get template() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.durationInSeconds}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const div = document.createElement('div');
+    div.innerHTML = this.template;
+
+    this.element = div.firstElementChild;
+  }
+
+  show(outerElement) {
+    if (outerElement) {
+      outerElement.append(this.element);
+    }
+    setTimeout(this.remove, this.duration);
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
 }
